8fbb133abc943405e15dc41db7bc26f72b38941b
rytoukai <rytoukai@gmail.com>
2023-08-30 15:37:33 -0400

    Update swapContract Sample.sol

--- Solidity Samples/swapContract Sample.sol
+++ Solidity Samples/swapContract Sample.sol
@@ -1,106 +1,165 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8;
-pragma abicoder v2;
+pragma solidity ^0.8.10;
 
+import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./utils/aave/FlashLoanReceiverBase.sol";
-import "./utils/aave/Interfaces.sol";
 
-contract MHS is Ownable {
+contract UniPancake is Ownable, IFlashLoanRecipient {
     ISwapRouter internal constant uniswapRouter =
         ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    ISwapRouter internal constant pancakeRouter = 
+        ISwapRouter(0x1b81D678ffb9C0263b24A97847620C99d213eB14);
+    IVault internal constant vault = 
+        IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
 
     using SafeMath for uint256;
 
-    address public router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    address tokenIn;
-    address tokenSwap1;
-    address tokenSwap2;
-    uint24 fee1;
-    uint24 fee2;
-    uint24 fee3;
+    struct SwapParams{
+        bool direction;
+        address token0;
+        address token1;
+        uint24 poolFeeU;
+        uint24 poolFeeP;
+        uint256 amountIn; 
+    }
 
-    function setContract(address _contract) external onlyOwner {
-        tokenIn = _contract;
+    function _swapU2P(
+        address tokenIn,
+        address tokenOut,
+        uint24 poolFeeU,
+        uint24 poolFeeP,
+        uint256 amountIn
+        ) internal returns (uint256) {
+
+        ISwapRouter.ExactInputSingleParams memory paramsU = 
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: tokenIn,
+                tokenOut: tokenOut,
+                fee: poolFeeU,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        uint256 amountOut = uniswapRouter.exactInputSingle(paramsU);
+
+        ISwapRouter.ExactInputSingleParams memory paramsP = 
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: tokenOut,
+                tokenOut: tokenIn,
+                fee: poolFeeP,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: amountOut,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        return pancakeRouter.exactInputSingle(paramsP);
+    }
+
+    function _swapP2U(
+        address tokenIn,
+        address tokenOut,
+        uint24 poolFeeU,
+        uint24 poolFeeP,
+        uint256 amountIn 
+        ) internal returns (uint256) {
+
+        ISwapRouter.ExactInputSingleParams memory paramsP = 
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: tokenIn,
+                tokenOut: tokenOut,
+                fee: poolFeeP,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        uint256 amountOut = pancakeRouter.exactInputSingle(paramsP);
+
+        ISwapRouter.ExactInputSingleParams memory paramsU = 
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: tokenOut,
+                tokenOut: tokenIn,
+                fee: poolFeeU,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: amountOut,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        return uniswapRouter.exactInputSingle(paramsU);
     }
 
     function approval(
         address _contract1,
         address _contract2,
-        uint256 amounts
-    ) external onlyOwner {
+        uint256 amounts ) external onlyOwner {
         IERC20(_contract1).approve(_contract2, amounts);
     }
 
-    function checkBalance() internal view returns (uint256) {
-        return IERC20(tokenIn).balanceOf(address(this));
+    function routerApproval(
+        address _erc20Token,
+        uint256 amounts ) external onlyOwner {
+            IERC20(_erc20Token).approve(0xE592427A0AEce92De3Edee1F18E0157C05861564, amounts);
+            IERC20(_erc20Token).approve(0x1b81D678ffb9C0263b24A97847620C99d213eB14, amounts);
     }
 
-    function withdraw() public onlyOwner {
-        IERC20(tokenIn).transferFrom(
+    function checkBalance(address token) internal view returns (uint256) {
+        return IERC20(token).balanceOf(address(this));
+    }
+
+    function withdraw(address token) external onlyOwner {
+        IERC20(token).transferFrom(
             address(this),
             payable(msg.sender),
-            checkBalance()
+            checkBalance(token)
         );
     }
 
-    function setParams(
-        address token1,
-        address token2,
-        address token3,
-        uint24 fee12,
-        uint24 fee23,
-        uint24 fee31
-    ) external {
-        tokenIn = token1;
-        tokenSwap1 = token2;
-        tokenSwap2 = token3;
-        fee1 = fee12;
-        fee2 = fee23;
-        fee3 = fee31;
+    function makeFlashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        bytes memory userData
+    ) external onlyOwner{
+        vault.flashLoan(this, tokens, amounts, userData);
+        require(checkBalance(tokens[0]) > amounts[0] , "Less than");
     }
 
-    function _swap() internal returns (uint256 amountOut) {
-        ISwapRouter.ExactInputParams memory params = ISwapRouter
-            .ExactInputParams({
-                path: abi.encodePacked(
-                    tokenIn,
-                    fee1,
-                    tokenSwap1,
-                    fee2,
-                    tokenSwap2,
-                    fee3,
-                    tokenIn
-                ),
-                recipient: address(this),
-                deadline: block.timestamp,
-                amountIn: checkBalance(),
-                amountOutMinimum: 0
-            });
-        amountOut = uniswapRouter.exactInput(params);
-    }
+    function receiveFlashLoan(
+        IERC20[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory userData
+    ) external override {
+        require(msg.sender == 0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+        
+        SwapParams memory swapParams = abi.decode(userData, (SwapParams));
 
-    function executeOperation(
-        address[] calldata assets,
-        uint256[] calldata amounts,
-        uint256[] calldata premiums,
-        address initiator,
-        bytes calldata params
-    ) external returns (bool) {
-        _swap();
-
-        for (uint256 i = 0; i < assets.length; i++) {
-            uint256 amountOwing = amounts[i].add(premiums[i]);
-            IERC20(assets[i]).approve(
-                address(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9),
-                amountOwing
+        if (swapParams.direction) {
+            _swapU2P(
+                swapParams.token0,
+                swapParams.token1,
+                swapParams.poolFeeU,
+                swapParams.poolFeeP,
+                amounts[0]
+            );
+        } else {
+            _swapP2U(
+                swapParams.token0,
+                swapParams.token1,
+                swapParams.poolFeeU,
+                swapParams.poolFeeP,
+                amounts[0]
             );
         }
 
-        return true;
+        IERC20(tokens[0]).transfer(0xBA12222222228d8Ba445958a75a0704d566BF2C8, amounts[0]);
     }
-
-    receive() external payable {}
 }
